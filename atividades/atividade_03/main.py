73b34321a4534d3a9dfec82c9fd448657d08f58e
PauloNascimento414 <paulonascimento2003@gmail.com>
2025-06-16 21:45:52 -0300

    Aula 2 do dia 16/06/2025.

--- atividades/atividade_03/main.py
+++ atividades/atividade_03/main.py
@@ -17,33 +17,31 @@ while True:
     print("2 - Sair do programa")
 
     operador = input("Informe a operação desejada: ").strip()
+   
     
+    
+
     match operador:
         case "0":
-            try:    
-                raio = float(input("Informe o valor do raio: ").replace(",", "."))
-                area = {m.pi} * (raio ** 2)
+            try:
+                raio = float(input("Informe o valor do raio: ").replace(",", "."))    
+                area = m.pi * (raio ** 2)
                 print(f"A área do círculo com raio {raio} é: {area}")
             except Exception as e:
                 print(f"Não foi possível calcular a área. {e}.")
-            finally:
-                o.system('cls')
-                continue
+                break
         case "1":
             try:
                 raio = float(input("Informe o valor do raio: ").replace(",", "."))
-                circunferencia = 2 * 3.14 * raio
+                circunferencia = 2 * m.pi * raio
                 print(f"A circunferência do círculo com raio {raio} é: {circunferencia}")
             except Exception as e:
                 print(f"Não foi possível calcular a circunferência. {e}.")
-            finally:
-                o.system('cls')
                 continue
         case "2":
-            print("Saindo do programa...") 
+            print("Saindo do programa...")
             break
         case _:
             print("Operação inválida. Tente novamente.")
-            o.system('cls')
             continue
-
+    o.system("cls")
