8690b930eaa99b20f47780e03fa996b73aac69b6
PauloNascimento414 <paulonascimento2003@gmail.com>
2025-07-01 21:31:26 -0300

    Aula do dia 01/07/2025.

---
+++ manipulacao_arquivos/43_manipular_arquivos_parte_02/main.py
@@ -0,0 +1,31 @@
+#importações
+import os
+
+#entrada de dados
+while True:
+    try:
+        arquivo = input("Informe o nome do arquivo: ").strip().lower()
+
+        #abre o arquivo
+        with open(f"{arquivo}.txt", "r", encoding="utf-8") as f:
+            arquivo_aberto = f.read()
+        os.system("cls" if os.name == 'nt' else "clear")
+        # mostra os dados do arquivo
+        print(arquivo_aberto)
+
+        while True: 
+            prosseguir = input("Deseja abrir outor arquivo? (s/n)").strip().lower()  
+            if prosseguir == "s" or prosseguir == "n":
+                break
+            else:
+                print("Opção inválida.")
+                continue
+        match prosseguir:
+            case "s":
+                continue
+            case "n":
+                break
+
+    except Exception as e:
+        print(f"Não foi possível ler o arquivo. {e}.")
+        continue
